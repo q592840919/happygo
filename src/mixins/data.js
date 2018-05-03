6641f9f86630f26a670a87b904364384afca8c7b
q592840919 <xuefengmsa@163.com>
2018-05-03 18:19:46 +0800

    one

---
+++ src/mixins/data.js
@@ -0,0 +1,34 @@
+// const pages = {
+//   swipers:[
+//     {
+//       image: '/images/swipers/milk.png',
+//       url: ''
+//     }
+//   ],
+//   categorys: [
+//     {
+//       image: '/images/swipers/milk.png',
+//       url: '',
+//       title: '牛奶'
+//     }
+//   ],
+//   recommends: [
+//     {
+//       image: '/images/swipers/milk.png',
+//       url: ''
+//     }
+//   ],
+//   burstings: [
+//     {
+//       image: '/images/swipers/milk.png',
+//       title: '牛奶',
+//       info: '第二件半价||立享五折||买一送一||20元3包||满60送30',
+//       during: '5月10~5月20',
+//       collection: false
+//     }
+//   ]
+// }
+
+// export {
+//   pages
+// }
