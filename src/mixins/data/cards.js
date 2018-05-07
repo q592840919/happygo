4eecb4118bae70608b61dcf1eb9bab7de4155903
q592840919 <xuefengmsa@163.com>
2018-05-07 18:13:33 +0800

    finish index

---
+++ src/mixins/data/cards.js
@@ -0,0 +1,57 @@
+const cards = {
+  discounts: [
+    // {
+    //   name: "华润万家",
+    //   info: {
+    //     image: '/images/swipers/milk.png',
+    //     title: '蒙牛牛奶',
+    //     type: '微信支付专享',
+    //     content: '50元代金券',
+    //     during: '5月10~5月20',
+    //     requirement: '满100.01元可用',
+    //     lastTime: 1525687018000,
+    //     url: ""
+    //   }
+    // }
+    {
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '第二件半价',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '.立享五折',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '买一送一',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '20元3包',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '满60送30',
+      during: '5月10~5月20',
+      collection: true,
+      url: ""
+    }
+  ]
+}
+
+export {
+  cards
+}
---
+++ src/mixins/data/pages.js
@@ -0,0 +1,91 @@
+const pages = {
+  swipers:[
+    {
+      image: '/images/swipers/milk.png',
+      url: ''
+    },{
+      image: '/images/swipers/coffe.png',
+      url: ''
+    }
+  ],
+  categorys: [
+    {
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    },{
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    },{
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    },{
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    }
+  ],
+  recommends: [
+    {
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    },{
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    },{
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    },{
+      image: '/images/swipers/milk.png',
+      url: '',
+      title: '牛奶'
+    }
+  ],
+  burstings: [
+    {
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '第二件半价',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '.立享五折',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '买一送一',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '20元3包',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '满60送30',
+      during: '5月10~5月20',
+      collection: true,
+      url: ""
+    }
+  ]
+}
+
+export {
+  pages
+}
