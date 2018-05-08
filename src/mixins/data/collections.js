e369ed61b0834780df8d5c173dde41c42d90eb50
q592840919 <xuefengmsa@163.com>
2018-05-08 18:13:06 +0800

    finish one

---
+++ src/mixins/data/collections.js
@@ -0,0 +1,45 @@
+const collections = {
+  discounts: [
+  {
+    name: "华润万家",
+    id: 1,
+    data: [{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '第二件半价',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '.立享五折',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    }]
+  },{
+    name: "苏果",
+    id: 1,
+    data: [{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '第二件半价',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    },{
+      image: '/images/swipers/milk.png',
+      title: '牛奶',
+      price: '.立享五折',
+      during: '5月10~5月20',
+      collection: false,
+      url: ""
+    }]
+  }
+  ]
+}
+
+export {
+  collections
+}
---
+++ src/mixins/data/homes.js
@@ -0,0 +1,15 @@
+const homes = {
+  settings:[
+    {
+      title: '联系客服',
+      url: ''
+    },{
+      title: '商务合作',
+      url: ''
+    }
+  ]
+}
+
+export {
+  homes
+}
